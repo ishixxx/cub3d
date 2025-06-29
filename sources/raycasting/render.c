6aa66db93e872f1dbbccb1f365127272de79e43d
veron <ver0niqueg@LAPTOP-O1EHI99F.localdomain>
2025-06-29 19:55:34 +0200

    updated

--- sources/raycasting/render.c
+++ sources/raycasting/render.c
@@ -6,21 +6,16 @@
 /*   By: vgalmich <vgalmich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 18:04:24 by vgalmich          #+#    #+#             */
-/*   Updated: 2025/06/21 13:34:11 by vgalmich         ###   ########.fr       */
+/*   Updated: 2025/06/29 19:55:14 by vgalmich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
-/*
-- fonction pour le rendu du background avec display width et display height
-- fonction render avec render background + raycasting + mlx_put_image_to_window
-*/
-
 // fonctin qui cree une couleur
-int create_trgb(int t, int r, int g, int b)
+int create_trgb(int t, t_color color)
 {
-	return (t << 24 || r << 16 | g << 8 | b);
+	return (t << 24 | color.r << 16 | color.g << 8 | color.b);
 }
 
 // fonction pour remplir l'image de fond de la fenetre (avec les couleurs ciel + sol)
@@ -28,20 +23,23 @@ void	render_background(t_cub3d *cub)
 {
 	int x;
 	int y;
+	t_point p;
 
 	x = 0;
-	y = 0;
 	while (x < cub->win_width)
 	{
+		y = 0;
 		while (y < cub->win_height)
 		{
+			p.x = (double)x;
+			p.y = (double)y;
 			if (y < cub->win_height / 2) // si on est a la moitie haute de l'ecran
-				draw_colored_pixel(cub, x, y, create_trgb(0, cub->sky_rgb[0], cub->sky_rgb[1], cub->sky_rgb[2])); // coder ces 2 fonctions
+				draw_pixel(&cub->image, p, create_trgb(0, cub->ceiling.r, cub->ceiling.g, cub->ceiling.b)); // coder ces 2 fonctions
 			else
-				draw_colored_pixel(cub, x, y, create_trgb(0, cub->floor_rgb[0], cub->floor_rgb[1], cub->florr_rgb[2]));
+				draw_pixel(&cub->image, p, create_trgb(0, cub->floor.r, cub->floor.g, cub->floor.b));
 			y++;
 		}
-		++x
+		++x;
 	}	
 }
 
